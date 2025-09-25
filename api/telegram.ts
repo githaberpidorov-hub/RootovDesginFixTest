d3364e86fadf44dcd142ce89c9571da970685bc5
Sviat Dekich <githaberpidorov@gmail.com>
2025-09-25 20:43:22 +0300

    Initial Commit

--- api/telegram.ts
+++ api/telegram.ts
@@ -1,4 +1,4 @@
-import { getSupabase } from "./_supabase.js";
+import { getSupabase } from "./_supabase";
 
 // Serverless function to send a Telegram message using bot token and chat id
 // Configure environment variables in Vercel dashboard:
